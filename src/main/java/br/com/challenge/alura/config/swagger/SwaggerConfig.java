615b6da9ebe88a7ac1b3d2a3c08f5984067b903a
aronricardo93 <aronricardo@hotmail.com>
2022-10-05 10:25:39 -0300

    config swagger

--- src/main/java/br/com/challenge/alura/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/challenge/alura/config/swagger/SwaggerConfig.java
@@ -9,24 +9,29 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
 	  public Docket getBean() {
 	    return new Docket(DocumentationType.SWAGGER_2)
+	    	.apiInfo(getInfo())
 	    	.select()
 	    	.apis(RequestHandlerSelectors.any())
-	        .paths(PathSelectors.any())
-	        .build()
-	        .apiInfo(getInfo());
+	    	.paths(PathSelectors.any())
+	        .build();
 	  }
 	  
 	  private ApiInfo getInfo() {
 	    return new ApiInfoBuilder()
 	        .title("Controle Familiar API")
 	        .description("Sistema de controle familiar de receitas e despesas")
-	        .build();
+	        .version("1.0.0")
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+            .build();
 	  }
 }
