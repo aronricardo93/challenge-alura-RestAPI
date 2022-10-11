59c70e1c1c5b609a88dc152ea30503c03d7e045e
aronricardo93 <aronricardo@hotmail.com>
2022-10-11 10:20:35 -0300

    validação do token

--- src/main/java/br/com/challenge/alura/filter/JwtTokenAutenticacaoFiltro.java
+++ src/main/java/br/com/challenge/alura/filter/JwtTokenAutenticacaoFiltro.java
@@ -1,23 +1,53 @@
 package br.com.challenge.alura.filter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.challenge.alura.config.security.token.TokenService;
+import br.com.challenge.alura.models.Usuario;
+import br.com.challenge.alura.repositories.UsuarioRepository;
 
 public class JwtTokenAutenticacaoFiltro extends OncePerRequestFilter{
 
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
 		String token = recuperarToken(request);
 		
+		if(tokenService.isTokenValido(token)) {
+			autenticarCliente(token);
+		}
+		
 		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Optional<Usuario> usuario = repository.findById(idUsuario);
+		
+		
+		var authentication = new UsernamePasswordAuthenticationToken(usuario.get(), null, usuario.get().getAuthorities());
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
